403332dbbba5263b8074d10d5961f8b42a9f944f
Joystick <bubbalou112@gmail.com>
2022-03-20 21:26:35 -0400

    nice testing

---
+++ testingnice.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+    cout << "Please enter a number: ";
+    cin >> x;
+    cout << "You entered: " << x << "\n\n";
+
+    if(x == 69 && x == 420){
+        cout << "Nice\n\n";
+    }else{
+        cout << "Not nice\n\n";
+    }
+}
