938c9033d0a7a5a901d642fd02299f3c2abe1eec
Joystick <bubbalou112@gmail.com>
2022-03-21 22:00:06 -0400

    collaborate test

--- testingnice.cpp
+++ testingnice.cpp
@@ -1,14 +1,26 @@
 #include <iostream>
 using namespace std;
 
+
+
+string usrName()
+{
+    string name;
+    cout << "What is your name?: ";
+    cin >> name;
+    return name;
+}
+
 int main()
 {
+    string usrNames;
+    usrNames = usrName();
     int x;
-    cout << "Please enter a number: ";
+    cout << "Please enter a number, " << usrNames << ": ";
     cin >> x;
     cout << "You entered: " << x << "\n\n";
 
-    if(x == 69 && x == 420){
+    if(x == 69 || x == 420){
         cout << "Nice\n\n";
     }else{
         cout << "Not nice\n\n";
